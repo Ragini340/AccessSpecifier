a199cf6283ba68549b42642054e35f9e15acfb54
Ragini340 <88105537+Ragini340@users.noreply.github.com>
2022-11-30 12:09:57 +0530

    Add files via upload

---
+++ AccessSpecifierPOC/InternalPOC.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccessSpecifierPOC
+{
+    class InternalPOC
+    {
+        static void Main(string[] args)
+        {
+            InternalAccessSpecifier obj = new InternalAccessSpecifier();
+            // Accessing internal variable  
+            Console.WriteLine("Hi " + obj.name);
+
+            // Accessing internal function  
+            obj.Msg("Hi R ");
+        }
+    }
+}
